cb1aed0dc50d74671d62a45d370114faba8bf86f
MizuhoOkimoto <54873998+MizuhoOkimoto@users.noreply.github.com>
2021-09-16 17:18:05 -0400

    reorganized

--- pajama-ssg/pajama-ssg.js
+++ pajama-ssg/pajama-ssg.js
@@ -9,15 +9,10 @@ var argv = require("yargs/yargs")(process.argv.slice(2))
   .alias("s", "stylesheet")
   .version("v", "version", "v0.1.0").argv;
 
-// Node.js File System: it can read/create/delete/update/rename files
 var fs = require("fs");
-// var HTMLParser = require("node-html-parser");
 const prettier = require("prettier");
 
-// create new directory
 var path = "./dist";
-
-/*----- check if dist directory exist -----*/
 
 if (fs.existsSync("./dist")) {
   if (fs.existsSync("./dist")) {
@@ -25,20 +20,19 @@ if (fs.existsSync("./dist")) {
     if (files.length > 0) {
       files.forEach(function (filename) {
         if (fs.statSync(path + "/" + filename).isDirectory()) {
-          removeDir(path + "/" + filename); //delete the directory
+          removeDir(path + "/" + filename);
         } else {
-          fs.unlinkSync(path + "/" + filename); //delete the file
+          fs.unlinkSync(path + "/" + filename);
         }
       });
-      fs.rmdirSync(path); //deletes the entire directory recursively
+      fs.rmdirSync(path);
     } else {
       fs.rmdirSync(path);
     }
   }
-  // console.log("dist Directory already exists!");
 }
 
-// make the dist directory
+// Make the dist directory
 fs.mkdir("./dist", (err) => {
   if (err) {
     throw err;
@@ -46,23 +40,17 @@ fs.mkdir("./dist", (err) => {
   console.log("dist directory is created.");
 });
 
-/*----- Read files/lines -----*/
+// Read files/lines
 argv.i.forEach((input) => {
   var stats = fs.statSync(input);
   console.log("Is txt file in a directory ? " + stats.isDirectory());
 
-  /*----- STEP:1 if txt file(s) is in a directory -----*/
   if (stats.isDirectory()) {
-    console.log("im here");
     files = fs.readdirSync("./" + input);
 
     files.forEach((file) => {
-      var lines = []; //array for storing data
-
+      var lines = [];
       var title = file.split(".txt").shift();
-      console.log("Title:" + title);
-
-      // each line in the txt file executes and make it string
 
       fs.readFile(`${input}/${file}`, "utf8", (err, data) => {
         if (err) {
@@ -70,7 +58,6 @@ argv.i.forEach((input) => {
         } else {
           lines = data.toString().split(/\r?\n\r?\n/);
           var titleName = `<h1>${lines.shift()}</h1>`;
-          console.log("TitleName:" + titleName);
           var text = "";
           lines.forEach((line) => (text = text + `\n<p>${line}</p>`));
 
@@ -112,22 +99,15 @@ argv.i.forEach((input) => {
             if (err) {
               return console.log(err);
             } else {
-              console.log("HTML file(s) was saved!");
+              console.log(title + ".html was saved!");
             }
           });
         }
       });
     });
-    console.log("This is saved file:" + files);
   } else {
-    //if it is not under directory
-    var lines = []; //array for storing data
-
-    console.log(argv.i);
+    var lines = [];
     var title = input.split(".txt").shift();
-    console.log("Title:" + title);
-
-    // each line in the txt file executes and make it string
 
     fs.readFile(`${input}`, "utf8", (err, data) => {
       if (err) {
@@ -135,7 +115,6 @@ argv.i.forEach((input) => {
       } else {
         lines = data.toString().split(/\r?\n\r?\n/);
         var titleName = `<h1>${lines.shift()}</h1>`;
-        console.log("TitleName:" + titleName);
         var text = "";
         lines.forEach((line) => (text += `\n<p>${line}</p>`));
 
@@ -177,11 +156,10 @@ argv.i.forEach((input) => {
           if (err) {
             return console.log(err);
           } else {
-            console.log("HTML file(s) was saved!");
+            console.log(title + ".html was saved!");
           }
         });
       }
     });
-    console.log("file is not under a directory");
-  } //end stats.isDirectory()
-}); //end foreach
+  }
+});
