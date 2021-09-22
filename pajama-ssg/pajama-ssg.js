a4354d58e0baccb3be5e37c432cf501a9d02aa79
Anatoliy Serputov <aserputov@myseneca.ca>
2021-09-21 23:04:25 -0400

    Now you can work with .md files too

--- pajama-ssg/pajama-ssg.js
+++ pajama-ssg/pajama-ssg.js
@@ -46,8 +46,11 @@ argv.i.forEach((input) => {
   console.log("Is txt file in a directory ? " + stats.isDirectory());
 
   if (stats.isDirectory()) {
-    files = fs.readdirSync("./" + input);
 
+
+
+    files = fs.readdirSync("./" + input);
+console.log(files)
     files.forEach((file) => {
       var lines = [];
       var title = file.split(".txt").shift();
@@ -104,10 +107,21 @@ argv.i.forEach((input) => {
           });
         }
       });
-    });
+    }); 
+
+
+
+
   } else {
+
+
+    if(input.includes(".txt")){
+
+  
+
     var lines = [];
     var title = input.split(".txt").shift();
+  
 
     fs.readFile(`${input}`, "utf8", (err, data) => {
       if (err) {
@@ -161,5 +175,66 @@ argv.i.forEach((input) => {
         });
       }
     });
+
+  }
+  else{
+    
+    var lines = [];
+    var title = input.split(".md").shift();
+  
+
+    fs.readFile(`${input}`, "utf8", (err, data) => {
+      if (err) {
+        return console.log(err);
+      } else {
+        lines = data.toString().split(/\r?\n\r?\n/);
+        var titleName = `<h1>${lines.shift()}</h1>`;
+        var text = "";
+        lines.forEach((line) => (text += `\n<p>${line}</p>`));
+
+        if (argv.s == undefined) {
+          var template = `
+            <!doctype html>
+            <html lang="en">
+            <head>
+              <meta charset="utf-8">
+              <link rel="stylesheet" type="text/css" href="please add your css path" />
+              <title>${title}</title>
+              <meta name="viewport" content="width=device-width, initial-scale=1">
+            </head>
+            <body>
+              ${titleName}
+              ${text}
+            </body>
+            </html>
+            `;
+        } else {
+          var template = `
+              <!doctype html>
+              <html lang="en">
+              <head>
+                <meta charset="utf-8">
+                <link rel="stylesheet" type="text/css" href="${argv.s}" />
+                <title>${title}</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+              </head>
+              <body>
+                ${titleName}
+                ${text}
+              </body>
+              </html>
+              `;
+        }
+        const compTemp = prettier.format(template, { parser: "html" });
+        fs.writeFile(`./dist/${title}.html`, compTemp, function (err) {
+          if (err) {
+            return console.log(err);
+          } else {
+            console.log(title + ".html was saved!");
+          }
+        });
+      }
+    });
+  }
   }
 });
