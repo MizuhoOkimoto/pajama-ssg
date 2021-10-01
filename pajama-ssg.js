0f04c8e609ec11a8e69e947a613dd4fda480b4ad
MizuhoOkimoto <54873998+MizuhoOkimoto@users.noreply.github.com>
2021-10-01 17:26:21 -0400

    modify yargs

--- pajama-ssg.js
+++ pajama-ssg.js
@@ -1,14 +1,30 @@
 #!/usr/bin/env node
 var argv = require("yargs/yargs")(process.argv.slice(2))
   .example("$0 -i textFileName.txt")
-  .alias("i", "input")
+  .option('input', {
+    alias: 'i',
+    describe: 'input file'
+  })
   .array("i")
   .demandOption(["i"])
-  .help("h")
-  .alias("h", "help")
-  .alias("s", "stylesheet")
+  .option('help', {
+    alias: 'h',
+    describe: 'display command options'
+  })
+  .option('stylesheet', {
+    alias: 's',
+    describe: 'input the path for generating the stylesheet path'
+  })
+  .option('lang', {
+    alias: 's',
+    describe: 'input the language for the lang attribute'
+  })
+  .option('version', {
+    alias: 'v',
+    describe: 'this is version v0.1.0'
+  })
   .version("v", "version", "pajama-ssg v0.1.0")
-  .alias("l","lang").argv;
+  .argv;
 
 var fs = require("fs");
 const prettier = require("prettier");
