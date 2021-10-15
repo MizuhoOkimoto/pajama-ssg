9a1ea5482d954657f8bcd18408092b7c668cf1c6
MizuhoOkimoto <54873998+MizuhoOkimoto@users.noreply.github.com>
2021-10-15 17:06:14 -0400

    Add yargsConfig file
    Add a function and make tempGenerator
    
    replace code to call function

--- pajama-ssg.js
+++ pajama-ssg.js
@@ -1,42 +1,12 @@
-#!/usr/bin/env node
-var argv = require("yargs/yargs")(process.argv.slice(2))
-  .example("$0 -i textFileName.txt")
-  .option('input', {
-    alias: 'i',
-    describe: 'input file'
-  })
-  .array("i")
-  //.demandOption(["i"])
-  .option('help', {
-    alias: 'h',
-    describe: 'display command options'
-  })
-  .option('stylesheet', {
-    alias: 's',
-    describe: 'input the path for generating the stylesheet path'
-  })
-  .option('lang', {
-    alias: 'l',
-    describe: 'input the language for the lang attribute'
-  }) 
-  .option('config', {
-    alias: 'c',
-    describe: "Read values from a Config JSON file",
-  })
-
-  .option('version', {
-    alias: 'v',
-    describe: 'this is version v0.1.0'
-  })
-  .version("v", "version", "pajama-ssg v0.1.0")
-  .argv;
-
+var argv = require("./yargsConfig");
 var fs = require("fs");
 const prettier = require("prettier");
-
 const path = require("path");
-
 var distPath = "./dist";
+
+var tempGenerate = require("./tempGenerator");
+
+console.log(argv.i);
 
 if (fs.existsSync(distPath)) {
   if (fs.existsSync(distPath)) {
@@ -105,41 +75,9 @@ return;
           var titleName = `<h1>${lines.shift()}</h1>`;
           var text = "";
           lines.forEach((line) => (text = text + `\n<p>${line}</p>`));
-          var language = argv.l?argv.l:"en-CA";
 
-          if (argv.s == undefined) {
-            var template = `
-            <!doctype html>
-            <html lang="${language}">
-            <head>
-              <meta charset="utf-8">
-              <link rel="stylesheet" type="text/css" href="please_add_your_css_path" />
-              <title>${title}</title>
-              <meta name="viewport" content="width=device-width, initial-scale=1">
-            </head>
-            <body>
-              ${titleName}
-              ${text}
-            </body>
-            </html>
-            `;
-          } else {
-            var template = `
-              <!doctype html>
-              <html lang="${language}">
-              <head>
-                <meta charset="utf-8">
-                <link rel="stylesheet" type="text/css" href="${argv.s}" />
-                <title>${title}</title>
-                <meta name="viewport" content="width=device-width, initial-scale=1">
-              </head>
-              <body>
-                ${titleName}
-                ${text}
-              </body>
-              </html>
-              `;
-          }
+          let template = tempGenerate(argv.s, argv.l, title, titleName, text);
+
           const compTemp = prettier.format(template, { parser: "html" });
           fs.writeFile(`./dist/${title}.html`, compTemp, function (err) {
             if (err) {
@@ -152,11 +90,9 @@ return;
         }
       });
     }else{
-      
       var lines = [];
       var title = file.split(".md").shift();
      
-      var template ;
       fs.readFile(`${input}/${file}`, "utf8", (err, data) => {
         if (err) {
           return console.log(err);
@@ -185,48 +121,9 @@ return;
             else {
               text += `\n<p>${line}</p>`;
             }
-            
-            // console.log(`1 ${line}`)
-  
-            if (argv.s == undefined) {
-               template = `
-                <!doctype html>
-                <html lang="en">
-                <head>
-                  <meta charset="utf-8">
-                  <link rel="stylesheet" type="text/css" href="please add your css path" />
-                  <title></title>
-                  <meta name="viewport" content="width=device-width, initial-scale=1">
-                </head>
-                <body>
-                
-                  ${text}
-                </body>
-                </html>
-                `;
-            } else {
-               template = `
-                  <!doctype html>
-                  <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <link rel="stylesheet" type="text/css" href="${argv.s}" />
-                    <title></title>
-                    <meta name="viewport" content="width=device-width, initial-scale=1">
-                  </head>
-                  <body>
-                 
-                    ${text}
-                  </body>
-                  </html>
-                  `;
-            }
-  
-  
-  
+            //replaced
+            template = tempGenerate(argv.s, argv.l, title, titleName, text);
           })
-
-
 
           const compTemp = prettier.format(template, { parser: "html" });
           fs.writeFile(`./dist/${title}.html`, compTemp, function (err) {
@@ -238,27 +135,16 @@ return;
           });
         }
       });
-
-
-
     }
-
-
     });
-
-
     
   }  else {
 
-
   if(input.includes(".txt")){
-
-  
 
     var lines = [];
     var title = input.split(".txt").shift();
   
-
     fs.readFile(`${input}`, "utf8", (err, data) => {
       if (err) {
         console.log("Error:Something went wrong when read the text file");
@@ -268,41 +154,10 @@ return;
         var titleName = `<h1>${lines.shift()}</h1>`;
         var text = "";
         lines.forEach((line) => (text += `\n<p>${line}</p>`));
-        var language = argv.l?argv.l:"en-CA";
 
-        if (argv.s == undefined) {
-          var template = `
-            <!doctype html>
-            <html lang="${language}">
-            <head>
-              <meta charset="utf-8">
-              <link rel="stylesheet" type="text/css" href="please_add_your_css_path" />
-              <title>${title}</title>
-              <meta name="viewport" content="width=device-width, initial-scale=1">
-            </head>
-            <body>
-              ${titleName}
-              ${text}
-            </body>
-            </html>
-            `;
-        } else {
-          var template = `
-              <!doctype html>
-              <html lang="${language}">
-              <head>
-                <meta charset="utf-8">
-                <link rel="stylesheet" type="text/css" href="${argv.s}" />
-                <title>${title}</title>
-                <meta name="viewport" content="width=device-width, initial-scale=1">
-              </head>
-              <body>
-                ${titleName}
-                ${text}
-              </body>
-              </html>
-              `;
-        }
+        //replaced
+        let template = tempGenerate(argv.s, argv.l, title, titleName, text);
+
         const compTemp = prettier.format(template, { parser: "html" });
         fs.writeFile(`./dist/${title}.html`, compTemp, function (err) {
           if (err) {
@@ -320,7 +175,7 @@ return;
     var lines = [];
     var title = input.split(".md").shift();
    
-    var template ;
+    var template;
     fs.readFile(`${input}`, "utf8", (err, data) => {
       // console.log(data)
       if (err) {
@@ -350,52 +205,10 @@ return;
             text += `\n<p>${line}</p>`;
           }
           
-          // console.log(`1 ${line}`)
-
-          if (argv.s == undefined) {
-             template = `
-              <!doctype html>
-              <html lang="en">
-              <head>
-                <meta charset="utf-8">
-                <link rel="stylesheet" type="text/css" href="please add your css path" />
-                <title></title>
-                <meta name="viewport" content="width=device-width, initial-scale=1">
-              </head>
-              <body>
-              
-                ${text}
-              </body>
-              </html>
-              `;
-          } else {
-             template = `
-                <!doctype html>
-                <html lang="en">
-                <head>
-                  <meta charset="utf-8">
-                  <link rel="stylesheet" type="text/css" href="${argv.s}" />
-                  <title></title>
-                  <meta name="viewport" content="width=device-width, initial-scale=1">
-                </head>
-                <body>
-               
-                  ${text}
-                </body>
-                </html>
-                `;
-          }
-
-
-
+          //replaced
+          template = tempGenerate(argv.s, argv.l, title, "", text);
         })
 
-
-       
-        
-        
-
-       
         const compTemp = prettier.format(template, { parser: "html" });
         fs.writeFile(`./dist/${title}.html`, compTemp, function (err) {
           if (err) {
---
+++ tempGenerator.js
@@ -0,0 +1,30 @@
+//import argv from './yargsConfig';
+module.exports = tempGenerate = (argv_s, argv_l, title, titleName="", text) => {
+    let style = argv_s ? `<link rel="stylesheet" type="text/css" href="${argv_s}" />` : `<link rel="stylesheet" type="text/css" href="please_add_your_css_path" />`;
+    let lang = argv_l ? `<html lang="${argv_l}">`: `<html lang="en-CA">`;
+    let titleLine = title ? `<title>${title}</title>` : "";
+
+    var template = `
+            <!doctype html>
+            ${lang}
+            <head>
+              <meta charset="utf-8">
+              ${style}
+              ${titleLine}
+              <meta name="viewport" content="width=device-width, initial-scale=1">
+            </head>
+            <body>
+              ${titleName}
+              ${text}
+            </body>
+            </html>
+            `;
+            return template;
+}
+
+
+
+
+
+
+        
